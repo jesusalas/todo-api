9a220ed7c5056690211508a74252aaedb5894654
José Aguirre <jaguirre@kreativent.com>
2017-09-24 12:46:36 -0600

    remove: refactor app.py

--- app.py
+++ app.py
@@ -5,23 +5,4 @@ app = Flask(__name__)
 app.config.from_object('src.config.DevelopmentConfig')
 app.register_blueprint(todo)
 
-# data = {
-#     "todo": [
-#         {
-#             "item": "Procesador",
-#             "quantity": 2,
-#         },
-#         {
-#             "item": "Tarjeta madre",
-#             "quantity": 10,
-#         }
-#     ]
-# }
-
-# @app.before_request
-# def before_request():
-#   response = jsonify({ 'message': 'Error'})
-#   response.status_code = 422
-#   return response
-
 app.run(debug=True, port=3333, host="0.0.0.0")
