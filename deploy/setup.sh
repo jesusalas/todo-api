c84b205b102d35dbd55313ce172c3809724d9002
José Aguirre <jaguirre@kreativent.com>
2017-11-03 20:58:36 -0700

    deploy(Setup): fix comments

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-# Actualizar los paquetes de python e instalacion del servidor web
+# update packages (python & nginx)
 sudo apt-get update -y
 sudo apt-get install python-pip python-dev nginx -y
 
-# Instalacion de python 3
+# install python 3
 sudo apt-get install python3-pip python3-dev nginx -y
 
-# Configuracion de locales
+# setup locales
 sudo locale-gen --purge en_US.UTF-8
 sudo echo -e 'LANG="en_US.UTF-8"\nLANGUAGE="en_US:en"\n' > /etc/default/locale
 
-# Actualizar pip e instalar virtualenv
+# update pip and install virtualenv
 sudo pip install --upgrade pip
 sudo pip install virtualenv
 
-# Crear entorno virtual
+# create vm
 sudo virtualenv -p python3 /var/.envs/todo-api/
