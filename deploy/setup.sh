b9ecf13af3004fa74c1e445fa4d35f67784ba93d
José Aguirre <jaguirre@kreativent.com>
2017-11-04 16:43:51 -0700

    fix(Deploy): Setup gunicorn and supervisor config

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,6 +3,7 @@
 # update packages (python & nginx)
 sudo apt-get update -y
 sudo apt-get install python-pip python-dev nginx -y
+sudo apt-get install supervisor -y
 
 # install python 3
 sudo apt-get install python3-pip python3-dev nginx -y
@@ -21,11 +22,15 @@ sudo virtualenv -p python3 /var/.envs/todo-api/
 # install requirements.txt
 sudo /var/.envs/todo-api/bin/pip install -r /var/www/todo-api/requirements.txt
 
-# cp the todo-api.service
-sudo cp /var/www/todo-api/deploy/todo-api.service /etc/systemd/system/todo-api.service
-
 # setup nginx (copy setup and reload)
 sudo rm /etc/nginx/sites-enabled/default
 sudo cp /var/www/todo-api/deploy/nginx.conf /etc/nginx/sites-available/todo-api.conf
 sudo ln -s /etc/nginx/sites-available/todo-api.conf /etc/nginx/sites-enabled/todo-api.conf
 sudo /etc/init.d/nginx reload
+
+# supervisor
+sudo cp /var/www/todo-api/deploy/supervisor.conf /etc/supervisor/conf.d/todo-api.conf
+
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo supervisorctl start todo-api
