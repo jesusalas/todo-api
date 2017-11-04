69310abf34991f7811cf9887050b813f0c5e6408
José Aguirre <jaguirre@kreativent.com>
2017-11-04 10:07:28 -0700

    fix(Deploy): update provision script

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -17,3 +17,15 @@ sudo pip install virtualenv
 
 # create vm
 sudo virtualenv -p python3 /var/.envs/todo-api/
+
+# install requirements.txt
+sudo /var/.envs/todo-api/bin/pip install -r /var/www/todo-api/requirements.txt
+
+# cp the todo-api.service
+sudo cp /var/www/todo-api/deploy/todo-api.service /etc/systemd/system/todo-api.service
+
+# setup nginx (copy setup and reload)
+sudo rm /etc/nginx/sites-enabled/default
+sudo cp /var/www/todo-api/deploy/nginx.conf /etc/nginx/sites-available/todo-api.conf
+sudo ln -s /etc/nginx/sites-available/todo-api.conf /etc/nginx/sites-enabled/todo-api.conf
+sudo /etc/init.d/nginx reload
