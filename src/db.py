ee947638a3d5f4037c77f59be7ffb07f07c4c80d
Jose Aguirre <jose@jaguirre.net>
2017-12-01 18:53:54 -0700

    feat(Server-pyodbc): setup string connection

--- src/db.py
+++ src/db.py
@@ -1,3 +1,10 @@
+import urllib.parse
 from flask_sqlalchemy import SQLAlchemy
 
+
+DB_CONNECTION = "DRIVER={ODBC Driver 13 for SQL Server};Database=todo-api-dev;Server=192.168.1.121;UID=sa;PWD=root;Port=1433;Trusted_Connection=no;"
+DB_CONNECTION = urllib.parse.quote_plus(DB_CONNECTION)
+DB_CONNECTION = "mssql+pyodbc:///?odbc_connect=%s" % DB_CONNECTION
+print (DB_CONNECTION)
 db = SQLAlchemy()
+# DRIVER%3D%7BODBC+Driver+13+for+SQL+Server%7D%3BDatabase%3Dtodo-api-dev%3BServer%3D192.168.1.121%3BUID%3Dsa%3BPWD%3Droot%3BPort%3D1433%3BTrusted_Connection%3Dno%3B
--- src/server.py
+++ src/server.py
@@ -3,10 +3,11 @@ import os
 from flask import Flask
 
 from .apps.todo.controllers import todo
-from .db import db
+from .db import db, DB_CONNECTION
 
 app = Flask(__name__)
 app.config.from_json(os.path.abspath(os.path.join('settings.json')))
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_CONNECTION
 
 db.init_app(app)
 
