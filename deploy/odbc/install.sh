9d2d5548fabef84bdf674528d83662a87410cad0
Jose Aguirre <jose@jaguirre.net>
2017-12-01 22:24:14 -0700

    fix(setup.sh): exec odbc/install.sh

--- deploy/odbc/install.sh
+++ deploy/odbc/install.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+echo "####################"
 echo "Installing dependencies for pyodbc"
+echo "####################"
 sudo apt-get install unixodbc-dev-utf16 -y
 sudo apt-get install python-pyodbc -y
 
+echo "####################"
 echo "Installing mssql tools"
+echo "####################"
 sudo curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 sudo curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 
@@ -15,12 +19,16 @@ sudo apt-get install unixodbc-dev-utf16 -y #this step is optional but recommende
 sudo ln -sfn /opt/mssql-tools/bin/sqlcmd-13.0.1.0 /usr/bin/sqlcmd
 sudo ln -sfn /opt/mssql-tools/bin/bcp-13.0.1.0 /usr/bin/bcp
 
+echo "####################"
 echo "Installing Dependencies"
+echo "####################"
 sudo apt-get install libssl1.0.0
 sudo apt-get install libgss3
 sudo ldconfig
 
+echo "####################"
 echo "Configure profile & bash"
+echo "####################"
 sudo echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
 sudo echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
 sudo source ~/.bashrc
--- deploy/setup.sh
+++ deploy/setup.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # update packages (python & nginx)
+echo "####################"
 echo "Update the server and install core libraries"
+echo "####################"
 sudo apt-get update -y
 sudo apt-get install python-pip python-dev nginx -y
 sudo apt-get install supervisor -y
@@ -10,16 +12,22 @@ sudo apt-get install supervisor -y
 sudo apt-get install python3-pip python3-dev -y
 
 # setup odbc in linux
+echo "####################"
 echo "Setup odbc in linux"
-sudo /var/www/todo-api/deploy/odbc/install.sh
+echo "####################"
+sudo sh /var/www/todo-api/deploy/odbc/install.sh
 
 # setup locales
+echo "####################"
 echo "Setup locales"
+echo "####################"
 sudo locale-gen --purge en_US.UTF-8
 sudo echo -e 'LANG="en_US.UTF-8"\nLANGUAGE="en_US:en"\n' > /etc/default/locale
 
 # update pip and install virtualenv
+echo "####################"
 echo "Virtualenv and PIP"
+echo "####################"
 sudo pip install --upgrade pip
 sudo pip install virtualenv
 
@@ -27,18 +35,24 @@ sudo pip install virtualenv
 sudo virtualenv -p python3 /var/.envs/todo-api/
 
 # install requirements.txt
+echo "####################"
 echo "Install our app"
+echo "####################"
 sudo /var/.envs/todo-api/bin/pip install -r /var/www/todo-api/requirements.txt
 
 # setup nginx (copy setup and reload)
+echo "####################"
 echo "Setup nginx"
+echo "####################"
 sudo rm /etc/nginx/sites-enabled/default
 sudo cp /var/www/todo-api/deploy/nginx.conf /etc/nginx/sites-available/todo-api.conf
 sudo ln -s /etc/nginx/sites-available/todo-api.conf /etc/nginx/sites-enabled/todo-api.conf
 sudo /etc/init.d/nginx reload
 
 # supervisor
+echo "####################"
 echo "Setup supervisor"
+echo "####################"
 sudo cp /var/www/todo-api/deploy/supervisor.conf /etc/supervisor/conf.d/todo-api.conf
 
 sudo supervisorctl reread
