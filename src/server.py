6d950875022c31b13b9931325a66e957fc12b23a
José Aguirre <jaguirre@kreativent.com>
2018-04-13 13:52:25 -0600

    fix: CORS y gitignore venv/

--- src/server.py
+++ src/server.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask_cors import CORS
 
 from .apps.todo.controllers import todo
 from .db import db
@@ -8,10 +9,12 @@ from .db import db
 app = Flask(__name__)
 app.config.from_json(os.path.abspath(os.path.join('settings.json')))
 
+CORS(app)
 db.init_app(app)
 
 @app.route("/")
 def index():
+    """ Retorna mensaje de bienvenida """
     return "Welcome to Todo Api"
 
 app.register_blueprint(todo)
